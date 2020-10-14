2e086526e57ac0401f981dca3fb538efc0da29c2
Mikhail <mikl.a.z1998@gmail.com>
2020-10-14 13:47:51 +0300

    Lesson 2
    1. Настроить слежение за перемещением в фоне.
    
    2. Добавить кнопки «Начать новый трек» и «Закончить трек».
    
    3. При нажатии на «Начать новый трек»:
    a. Запускается слежение.
    b. Создаётся новая линия на карте или заменяется предыдущая.
    c. При получении новой точки она добавляется в маршрут.
    
    4. Добавить в приложение базу данных Realm.
    
    5. При нажатии на «Закончить трек»:
    a. Завершается слежение.
    b. Все точки маршрута сохраняются в базу данных.
    c. Прежде чем сохранить точки из базы, необходимо удалить предыдущие точки.
    
    6. Добавить кнопку «Отобразить предыдущий маршрут».
    
    7. При нажатии на «Отобразить предыдущий маршрут».
    a. Если в данный момент происходит слежение, то появляется уведомление о том, что сначала необходимо остановить слежение. С кнопкой «ОК», при нажатии на которую останавливается слежение, как если бы пользователь нажал на «Закончить трек».
    b. Загружаются точки из базы.
    c. На основе загруженных точек строится маршрут.
    d. Фокус на карте устанавливается таким образом, чтобы был виден весь маршрут.
    
    -----------------------
    + Добавлены кнопки которые вкл/выкл показ местоположения пользователя и трафика

--- ProjectMap/ProjectMap/AppDelegate.swift
+++ ProjectMap/ProjectMap/AppDelegate.swift
@@ -8,15 +8,43 @@
 
 import UIKit
 import GoogleMaps
+//import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    //var timer: Timer?
+    //var beginBackgroundTask: UIBackgroundTaskIdentifier?
+    //var timerCount = 10
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyBYO_jJYjF6EnHl8hN_QsSKxBjcwqtUI3c")
+        //GMSPlacesClient.provideAPIKey("AIzaSyBYO_jJYjF6EnHl8hN_QsSKxBjcwqtUI3c")
+        
+        
+        /*beginBackgroundTask = UIApplication.shared.beginBackgroundTask(expirationHandler: {
+            [weak self] in
+            guard let strongSelf = self else {return}
+            
+            UIApplication.shared.endBackgroundTask(strongSelf.beginBackgroundTask!)
+            strongSelf.beginBackgroundTask = UIBackgroundTaskIdentifier.invalid
+        })
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
+            [weak self] (_) in
+            //print(Date())
+            if self?.timerCount == 0 {
+                self?.timer?.invalidate()
+                UIApplication.shared.endBackgroundTask((self?.beginBackgroundTask!)!)
+                self?.beginBackgroundTask = UIBackgroundTaskIdentifier.invalid
+            } else {
+                self?.timerCount -= 1
+            }
+            
+        })*/
+        
+        
         return true
     }
 
--- ProjectMap/ProjectMap/SceneDelegate.swift
+++ ProjectMap/ProjectMap/SceneDelegate.swift
@@ -33,8 +33,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        /*let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(blurEffectView)*/
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
